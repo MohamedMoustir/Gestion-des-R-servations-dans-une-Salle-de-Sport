6fc9c532009b7189983ac4fe84709c2f9d00d146
MohamedMoustir <itsmoustir@gmail.com>
2024-12-07 22:58:04 +0100

    Add Openfrom function to show and style Réserver button

---
+++ script/main.js
@@ -0,0 +1,7 @@
+
+
+function Openfrom() {
+    document.getElementById("Réserver").classList.add("fixed", "top-28", "bg-white", "left-[50%]", "-translate-x-2/4");
+    document.getElementById("Réserver").classList.remove("hidden");
+   document.getElementById("continer").style.filter="blur(6px)"
+}
